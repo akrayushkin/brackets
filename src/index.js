31699924a77ece4a9b2b83ccf98d808acd815db1
Anton <akrayushkin@yandex.ru>
2020-03-04 01:52:59 +0300

    feat: end "Brackets" task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const openingBrackets = bracketsConfig.map( item => item[0] );
+  const closingBrackets = bracketsConfig.map( item => item[1] );
+  const arr = str.split('');
+  for (let i = 0; i < arr.length; i++) {
+    if (stack.length === 0 && closingBrackets.includes(arr[i]) && !openingBrackets.includes(arr[i])) return false;
+    if (stack.length !== 0 && openingBrackets[closingBrackets.indexOf(arr[i])] === stack[stack.length-1]) {
+        stack.pop();
+    } else if(openingBrackets.includes(arr[i])) {
+        stack.push(arr[i]);
+    }
+  }
+  return stack.length === 0 ? true : false;
 }
